bd35dd2223610fe7cb40a0c5574a2a3dc3f573ff
Son <nguyenson18296@gmail.com>
2019-08-14 18:37:22 +0700

    Heroku app

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value;
+  fetch(`/weather?address=${location}`).then((res) => {
+    res.json().then((data) => {
+      if(data.error) {
+        messageOne.textContent = 'Error';
+      } else {
+        messageOne.textContent = data.location;
+        messageTwo.textContent = data.forecast;
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    })
+  })
+})
+
